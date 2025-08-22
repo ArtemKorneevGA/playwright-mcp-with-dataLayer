a960997ea89504eca5ba968282200fbdcfc5d530
Artem Korneev <dev.dandp@gmail.com>
2025-08-22 15:36:24 +0300

    feat: added getDataLayer tool

--- src/tools.ts
+++ src/tools.ts
@@ -14,24 +14,25 @@
  * limitations under the License.
  */
 
-import common from './tools/common.js';
-import console from './tools/console.js';
-import dialogs from './tools/dialogs.js';
-import evaluate from './tools/evaluate.js';
-import files from './tools/files.js';
-import install from './tools/install.js';
-import keyboard from './tools/keyboard.js';
-import navigate from './tools/navigate.js';
-import network from './tools/network.js';
-import pdf from './tools/pdf.js';
-import snapshot from './tools/snapshot.js';
-import tabs from './tools/tabs.js';
-import screenshot from './tools/screenshot.js';
-import wait from './tools/wait.js';
-import mouse from './tools/mouse.js';
+import common from "./tools/common.js";
+import console from "./tools/console.js";
+import dialogs from "./tools/dialogs.js";
+import evaluate from "./tools/evaluate.js";
+import files from "./tools/files.js";
+import install from "./tools/install.js";
+import keyboard from "./tools/keyboard.js";
+import navigate from "./tools/navigate.js";
+import network from "./tools/network.js";
+import pdf from "./tools/pdf.js";
+import snapshot from "./tools/snapshot.js";
+import tabs from "./tools/tabs.js";
+import screenshot from "./tools/screenshot.js";
+import wait from "./tools/wait.js";
+import mouse from "./tools/mouse.js";
+import getDataLayer from "./tools/getDataLayer.js";
 
-import type { Tool } from './tools/tool.js';
-import type { FullConfig } from './config.js';
+import type { Tool } from "./tools/tool.js";
+import type { FullConfig } from "./config.js";
 
 export const allTools: Tool<any>[] = [
   ...common,
@@ -49,8 +50,13 @@ export const allTools: Tool<any>[] = [
   ...snapshot,
   ...tabs,
   ...wait,
+  ...getDataLayer,
 ];
 
 export function filteredTools(config: FullConfig) {
-  return allTools.filter(tool => tool.capability.startsWith('core') || config.capabilities?.includes(tool.capability));
+  return allTools.filter(
+    (tool) =>
+      tool.capability.startsWith("core") ||
+      config.capabilities?.includes(tool.capability)
+  );
 }
